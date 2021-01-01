60f9b95cb14e7999349293c2306c789da27ee1db
Benjamin Marie Kamano <kamanobenjamin@gmail.com>
2021-01-01 21:47:56 +0000

    up

--- cms-online03/index.php
+++ cms-online03/index.php
@@ -49,7 +49,7 @@ session_start();
                        $post_author=$row['post_author'];
                        $post_date=$row['post_date'];
                        $post_image=$row['post_image'];
-                       $post_content= substr($row['post_content'],0,500);
+                       $post_content= substr($row['post_content'],0,100);
                        $post_status=$row['post_status'];
                        if ($post_status=='Actif') {
                          
