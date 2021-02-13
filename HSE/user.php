9d20cc54c447b5c9221719a727b30f5a6c875a57
Benjamin Marie Kamano <kamanobenjamin@gmail.com>
2021-02-13 08:24:17 +0000

    UP

--- HSE/user.php
+++
@@ -1,4 +0,0 @@
-<li class="button pull-right">
- <a href="#"><span class="username"><?php echo $_SESSION['nom'],"&nbsp;&nbsp;",$_SESSION['poste']; ?></span></a>                                               
- </li>
-  <li><a href="updates/contenu.php">RETOUR</a></li>
