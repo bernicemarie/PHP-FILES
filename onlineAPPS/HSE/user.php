ab5d2fd5063151223b85b7e224792cf428e540ff
Benjamin Tamba KAMANO <kamanobenjamin@gmail.com>
2021-06-06 11:45:51 +0000

    OK

--- onlineAPPS/HSE/user.php
+++
@@ -1,4 +0,0 @@
-<li class="button pull-right">
- <a href="#"><span class="username"><?php echo $_SESSION['nom'],"&nbsp;&nbsp;",$_SESSION['poste']; ?></span></a>                                               
- </li>
-  <li><a href="contenus/contenu.php">RETOUR</a></li>
